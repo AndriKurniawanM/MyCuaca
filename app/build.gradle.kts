7147d6aa7b5382f7572913300fcf9f09a6343375
AndriKurniawanM. <andrikurniawanm.12@gmail.com>
2025-07-18 21:13:49 +0800

    Bukankah Ini My...

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.mycuaca"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mycuaca"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation ("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.androidx.runtime.saved.instance.state)
+}
---
+++ app/src/main/java/com/example/mycuaca/Activitis/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.mycuaca.Activitis;
+
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mycuaca.Adapters.HourlyAdapters;
+import com.example.mycuaca.Domains.Hourly;
+import com.example.mycuaca.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends android.app.Activity {
+    private RecyclerView.Adapter adapterHourly;
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerview();
+    }
+
+    private void initRecyclerview(){
+        ArrayList<Hourly> item = new ArrayList<>();
+
+        item.add(new Hourly( "9 PM" , 28, "cloudy"));
+        item.add(new Hourly( "11 PM" , 29, "sunny"));
+        item.add(new Hourly( "12 AM" , 30, "wind"));
+        item.add(new Hourly( "1 AM" , 26, "rainy"));
+        item.add(new Hourly( "2 AM" , 24, "storm"));
+
+        recyclerView=findViewById(R.id.view1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
+
+        adapterHourly=new HourlyAdapters(item);
+        recyclerView.setAdapter(adapterHourly);
+
+
+
+    }
+
+}
