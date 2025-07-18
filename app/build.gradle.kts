fcf12bd1b5a0282258c263821bce2a2318cc4225
AndriKurniawanM. <andrikurniawanm.12@gmail.com>
2025-07-18 23:06:18 +0800

    Selesai mengerjakan UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,9 +29,8 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
-    implementation(libs.androidx.runtime.saved.instance.state)
 }
